9dbd121ef55020e6f78e197e67f96bff4273fe3e
JitChowDhury <jitchowdhury8717@gmail.com>
2026-04-19 22:55:19 +0530

    input script

---
+++ Assets/Scripts/Input/InputManager.cs
@@ -0,0 +1,56 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
+
+namespace HorrorGame.Manager
+{
+    public class InputManager : MonoBehaviour
+    {
+        [SerializeField] private PlayerInput playerInput;
+
+        public Vector2 Move { get; private set; }
+        public Vector2 Look { get; private set; }
+        public Vector2 Run { get; private set; }
+
+        private InputActionMap currentMap;
+        private InputAction moveAction;
+
+        private InputAction lookAction;
+
+        private void Awake()
+        {
+            currentMap = playerInput.currentActionMap;
+            moveAction = currentMap.FindAction("Move");
+            lookAction = currentMap.FindAction("Look");
+
+            moveAction.performed += onMove;
+            moveAction.performed += onLook;
+
+            moveAction.canceled += onMove;
+            moveAction.canceled += onLook;
+        }
+        private void onMove(InputAction.CallbackContext context)
+        {
+            Move = context.ReadValue<Vector2>();
+        }
+
+        private void onLook(InputAction.CallbackContext context)
+        {
+            Look = context.ReadValue<Vector2>();
+        }
+
+        void OnEnable()
+        {
+            currentMap.Enable();
+        }
+
+        void OnDisable()
+        {
+            currentMap.Disable();
+        }
+
+
+    }
+}
+
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace HorrorGame.PlayerController
+{
+    public class PlayerController : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
+
